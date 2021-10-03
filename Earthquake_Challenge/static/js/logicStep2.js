a4e5b6c9e943b70f44e9d3cfcdf139a7473ccea2
gabalita <gabalita@gmail.com>
2021-10-02 22:23:55 -0400

    adding challenge code

---
+++ Earthquake_Challenge/static/js/logicStep2.js
@@ -0,0 +1,161 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY,
+  }
+);
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY,
+  }
+);
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Light: streets,
+  Dark: satelliteStreets,
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map("mapid", {
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets],
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+// This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+function styleInfo(feature) {
+  return {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: "#ffae42",
+    color: "#000000",
+    radius: getRadius(feature),
+    stroke: true,
+    weight: 0.5,
+  };
+}
+
+// This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+function getRadius(feature) {
+  if (feature.properties.mag === 0) {
+    return 1;
+  }
+  return feature.properties.mag * 4;
+}
+
+// Retrieve the earthquake GeoJSON data.
+d3.json(
+  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+).then(function (data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data, {
+    // We turn each feature into a circleMarker on the map.
+
+    pointToLayer: function (feature, latlng) {
+      console.log(data);
+      return L.circleMarker(latlng);
+    },
+    // We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo,
+  }).addTo(map);
+});
+
+// // Grabbing our GeoJSON data.
+// d3.json(torontoHoods).then(function(data) {
+//   console.log(data);
+//   L.geoJSON(data, {
+//     style: myStyle,
+//     onEachFeature: function(feature, layer) {
+//       layer.bindPopup("<h3> Neigborhood: " + feature.properties.AREA_NAME + "</h3> <hr> <h3> Code: "
+//       + feature.properties.AREA_S_CD + "</h3>")}}).addTo(map);
+// });
+
+// Code for GeoJSON point
+// // Add GeoJSON data.
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
+
+// // Add a marker via the "pointToLayer" method
+// // Grabbing our GeoJSON data.
+// L.geoJson(sanFranAirport, {
+//     // We turn each feature into a marker on the map.
+//     pointToLayer: function(feature, latlng) {
+//       console.log(feature);
+//       return L.marker(latlng).bindPopup("<h2>" + feature.properties.city + "</h2>");
+//     }
+//   }).addTo(map);
+
+// Add a marker via the "onEachFeature" method
+// onEachFeature method
+//   L.geoJson(sanFranAirport, {
+//     onEachFeature: function(feature, layer) {
+//         console.log(feature)
+//         return layer.bindPopup("<h2>" + feature.properties.city + "</h2>");
+//      }
+// }).addTo(map);
+
+// // Code for Multiple Points Map
+// // Get data from cities.js
+// let cityData = cities;
+
+//   // Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location, {
+//         radius: city.population/100000}).bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//     .addTo(map);
+// });
+
+// Code for Mapping Lines
+// // Coordinates for each point to be used in the line.
+// // Coordinates for each point to be used in the polyline.
+// let line = [
+//     [33.9416, -118.4085],
+//     [37.6213, -122.3790],
+//     [40.7899, -111.9791],
+//     [47.4502, -122.3088]
+//   ];
+
+// // Create a polyline using the line coordinates and make the line black.
+// L.polyline(line, {
+//     color: "yellow"
+//  }).addTo(map);
